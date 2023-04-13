6862b685eb025694825cf754cf2a6d9e9398bc50
Awais Khalid <awais.khalid800@gmail.com>
2023-04-13 15:27:11 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,183 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <!-- Body Wrapper -->
+  <section class="wrapper container mx-auto py-20 px-12">
+        <h1 class="title text-center">
+            We make creative media <br/>that <span>adds value</span>
+        </h1>
+        <!-- GRID START -->
+        <div class="grid grid-cols-3 gap-6 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 media-grid mt-16">
+            <!-- CARD -->
+            <div class="card bg-base-100 shadow-xl">
+                <div class="grid grid-cols-2 gap-2">
+                    <div class="flex flex-col space-y-0.5 justify-center items-center h-80">
+                            <img src="https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&w=1000&q=80" class="img1" style="height:70%!important;" alt="home"/>
+
+                            <img src="https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&w=1000&q=80"  class="img2" style="height:70%!important;" alt="home"/>
+                    </div>
+                    <div class="right p-4 pt-10">
+                        <h2 class="card-title">Photography</h2>
+                        <ul class="mt-3">
+                            <li>
+                                Drone Photography
+                            </li>
+                            <li>
+                                Interiors
+                            </li>
+                            <li>
+                                Exteriors/ Architectural
+                            </li>
+                        </ul>
+                        <p class="mt-3">
+                            Every photograph we produce finds the beauty in your property while serving as a profitable asset.
+                        </p>
+                       
+                    </div>
+                </div>
+            </div>
+            <!-- CARD -->
+            <div class="card bg-base-100 shadow-xl">
+                <div class="grid grid-cols-2 gap-2">
+                    <div class="">
+                        <figure>
+                            <img src="https://i.pinimg.com/originals/3a/ae/d9/3aaed95bf7dbe6f63bda6a95c5406a1b.jpg" class="single" alt="home" />
+                        </figure>
+                    </div>
+                    <div class="right p-4 pt-10">
+                        <h2 class="card-title">Virtual Staging</h2>
+                        <p class="mt-3">
+                            Our staging will help you sell the potential of your space. We can work off of our own photography or photos you provide.
+                        </p>
+                       
+                    </div>
+                </div>
+            </div>
+            <!-- CARD -->
+            <div class="card bg-base-100 shadow-xl">
+                <div class="grid grid-cols-2 gap-2">
+                    <div class="flex flex-col space-y-0.5">
+                        <figure>
+                            <img src="https://i.pinimg.com/originals/3a/ae/d9/3aaed95bf7dbe6f63bda6a95c5406a1b.jpg"  class="single" alt="home" />
+                        </figure>
+                    </div>
+                    <div class="right p-4 pt-10">
+                        <h2 class="card-title">Renderings</h2>
+                        <ul class="mt-3">
+                            <li>
+                                Interiors 
+                            </li>
+                            <li>
+                                Exteriors
+                            </li>
+                        </ul>
+                        <p class="mt-3">
+                            Renderings are the gold standard in pre-construction marketing.
+                        </p>
+                       
+                    </div>
+                </div>
+            </div>
+            <!-- CARD -->
+            <div class="card bg-base-100 shadow-xl">
+                <div class="grid grid-cols-2 gap-2">
+                    <div class="flex flex-col space-y-0.5">
+                        <figure>
+                            <img src="https://i.pinimg.com/originals/3a/ae/d9/3aaed95bf7dbe6f63bda6a95c5406a1b.jpg"  class="single" alt="home" />
+                        </figure>
+                    </div>
+                    <div class="right p-4 pt-10">
+                        <h2 class="card-title">Interactive Tours</h2>
+                        <ul class="mt-3">
+                            <li>
+                                Matterport
+                            </li>
+                            <li>
+                                Video Walkthroughs
+                            </li>
+                        </ul>
+                        <p class="mt-3">
+                            Give your audience the ability to explore your property without requiring in-person travel.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- CARD -->
+            <div class="card bg-base-100 shadow-xl">
+                <div class="grid grid-cols-2 gap-2">
+                    <div class="flex flex-col space-y-0.5">
+                        <figure>
+                            <img src="https://i.pinimg.com/originals/3a/ae/d9/3aaed95bf7dbe6f63bda6a95c5406a1b.jpg"  class="single" alt="home" />
+                        </figure>
+                    </div>
+                    <div class="right p-4 pt-10">
+                        <h2 class="card-title">Floor Plans</h2>
+                        <ul class="mt-3">
+                            <li>
+                                On-site Measure
+                            </li>
+                            <li>
+                                2D Floor Plans
+                            </li>
+                            <li>
+                                3D Floor Plans
+                            </li>
+                        </ul>
+                        <p class="mt-3">
+                            We produce laser-precise floor plans faster than any other service.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- CARD -->
+            <div class="card bg-base-100 shadow-xl">
+                <div class="grid grid-cols-2 gap-2">
+                    <div class="flex flex-col space-y-0.5">
+                        <figure>
+                            <img src="https://i.pinimg.com/originals/3a/ae/d9/3aaed95bf7dbe6f63bda6a95c5406a1b.jpg"  class="single" alt="home" />
+                        </figure>
+                    </div>
+                    <div class="right p-4 pt-10">
+                        <h2 class="card-title">Video</h2>
+                        <ul class="mt-3">
+                            <li>
+                                Drone Cinematography
+                            </li>
+                            <li>
+                                Interiors
+                            </li>
+                            <li>
+                                Exteriors / Architectural
+                            </li>
+                        </ul>
+                        <p class="mt-3">
+                            Video content has higher engagement and will help your property stand out online.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- GRID ENDS -->
+
+        <div class="text-center">
+            <button class="btn btn-primary default mt-10">Get Started</button>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
